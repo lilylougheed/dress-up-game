bd3c370590bd4783beb3a6b87234d6f778fffcf4
lilylougheed <165317612+lilylougheed@users.noreply.github.com>
2026-01-06 02:20:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function changeShirt(num) {
+  document.getElementById("shirt").src = `images/shirt${num}.png`;
+}
+
+function changeSkirt(num) {
+  document.getElementById("skirt").src = `images/skirt${num}.png`;
+}
+
+function changeShoes(num) {
+  document.getElementById("shoes").src = `images/shoes${num}.png`;
+}
+
+function changeHair(num) {
+  document.getElementById("hair").src = `images/hair${num}.png`;
+}
